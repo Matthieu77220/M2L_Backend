53e585edeb258eb87cc02da5ccf426c0f6964881
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2025-11-19 15:05:26 +0100

    création des autres requetes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,47 @@
+import db from "../config/db.js"
+import bcrypt, { hash } from "bcrypt"
 
-// test récup depuis la routes
-const addAuth = (req,res) => {
-    res.json("Hello from controller")
+// ----- INSCRIPTION ----- //
+export const inscription = (req,res) => {
+    const sql = "SELECT * FROM adherent WHERE email = ?"
+    // const {email, password} = req.body
+    // Rajouter autre lors de l'inscription
+
+    db.query(sql, email, (err, results) => {
+        if (err) {
+            res.status(500).send("Erreur lors de las vérification des infos")
+        }else {
+            if (results.length) {
+                if (email === results[0].email) {
+                    res.send("Votre compte a déjà été crée.")
+                }
+            }else {
+                bcrypt.hash(password, 10, (err, hash) => {
+                    if (err) {
+                        res.status(500).send("Erreur lors du hashage du password")
+                    }else {
+                        // const sqlInscription = "INSERT INTO adherent () VALUES(?,?,?)"
+                        // Créer la BDD
+
+                        db.query(sqlInscription,[email,hash], (err,results) => {
+                            if (err) {
+                                res.status(500).send("Erreur lors de l'ajout de l'adherent dans la Base De Données")
+                            }else {
+                                res.status(200).send("User ajouté avec succès !")
+                            }
+                            
+                        })
+                    }
+                })
+            }
+        }
+    })
 }
 
-export default addAuth
+export const connexion = (req,res) => {
+
+}
+
+export const deconnexion = (req,res) => {
+
+}
