2807319a69a5b6f00fbdeb5d3134951d280405ae
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2025-11-19 14:20:15 +0100

    connexion a la BDD

---
+++ config/db.js
@@ -0,0 +1,16 @@
+// Import des dépendances
+import mysql2 from "mysql2"
+import 'dotenv/config'
+
+
+const db = mysql2.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+})
+
+export default db;
