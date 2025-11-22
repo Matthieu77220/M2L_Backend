18e77c0c8073dbe53723a9dd21a20306a4c58b28
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2025-11-22 13:20:34 +0100

    ajout de la fonctionnalité connexion

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,8 @@
 import db from "../config/db.js"
-import bcrypt, { hash } from "bcrypt"
+import bcrypt from "bcrypt"
 
 // ----- INSCRIPTION ----- //
-export const inscription = (req,res) => {
+export const inscription = (req, res) => {
     const sql = "SELECT * FROM adherent WHERE email = ?"
     // const {email, password} = req.body
     // Rajouter autre lors de l'inscription
@@ -10,26 +10,31 @@ export const inscription = (req,res) => {
     db.query(sql, email, (err, results) => {
         if (err) {
             res.status(500).send("Erreur lors de las vérification des infos")
-        }else {
+        } else {
             if (results.length) {
                 if (email === results[0].email) {
-                    res.send("Votre compte a déjà été crée.")
+                    res.send("Il existe déjà un adherent possédant le même email !")
                 }
-            }else {
+            } else {
                 bcrypt.hash(password, 10, (err, hash) => {
                     if (err) {
                         res.status(500).send("Erreur lors du hashage du password")
-                    }else {
+                    } else {
                         // const sqlInscription = "INSERT INTO adherent () VALUES(?,?,?)"
                         // Créer la BDD
 
-                        db.query(sqlInscription,[email,hash], (err,results) => {
+                        db.query(sqlInscription, [email, hash], (err, results) => {
                             if (err) {
                                 res.status(500).send("Erreur lors de l'ajout de l'adherent dans la Base De Données")
-                            }else {
-                                res.status(200).send("User ajouté avec succès !")
+
+                                // Vérifier le mdp hashé et celui de l'adherent
+                                bcrypt.compare(password, results[0].password_hash, (err, results) => {
+                                    if (err) {
+                                        res.status(500).send("Erreur lors ")
+                                    }
+                                })
                             }
-                            
+
                         })
                     }
                 })
@@ -38,10 +43,34 @@ export const inscription = (req,res) => {
     })
 }
 
-export const connexion = (req,res) => {
-
+export const connexion = (req, res) => {
+    const { email, password } = req.body
+    const sql = "SELECT * from adherent WHERE email = ? ;"
+    
+    db.query(sql, [email, password], (err, results) => {
+        if (err) {
+            res.status(500).send("Erreur lors de la recherche de l'adherent !")
+        }else {
+            if (email === results[0].email) {
+                // Vérifier si le mdp de l'user == mdp hashé dans la BDD
+                bcrypt.compare(password,results[0].password_hash, (err, results) => {
+                    if (err) {
+                        res.status(500).send("Erreur lors de la vérification du mot de passe !")
+                    }else {
+                        if (results) {
+                            res.send("Vous êtes connecté")
+                        }else {
+                            res.send("Mot de passe incorrect")
+                        }
+                    }
+                })
+            }else {
+                res.send("L'email n'existe pas !")
+            }
+        }
+    })
 }
 
-export const deconnexion = (req,res) => {
+export const deconnexion = (req, res) => {
 
 }
