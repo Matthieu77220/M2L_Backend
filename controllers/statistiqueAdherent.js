fb8d7a110d8fb4facbf07df76212ea427f07afc4
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2025-12-02 12:46:40 +0100

    ajout du controllers

---
+++ controllers/statistiqueAdherent.js
@@ -0,0 +1,11 @@
+import db from "../config/db.js"
+import 'dotenv/config'
+import jwt from "jsonwebtoken"
+
+// ----- Statistique ----- //
+export const statistique = (req, res) => {
+}
+
+// ----- Visualisation Match ----- //
+export const visualisationMatch = (req, res) => {
+}
