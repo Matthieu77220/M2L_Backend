af8e689cc2ea6fa055b73ca699a7885ded9f2a06
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2025-12-07 13:24:57 +0100

    réajustement des tables car pbl lors de la création des tables (foreign key)

--- BackEnd/tableSQL/tables.sql
+++ BackEnd/tableSQL/tables.sql
@@ -6,18 +6,28 @@ CREATE TABLE CLUB (
     email VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE UTILISATEUR (
-    id_utilisateur INT PRIMARY KEY AUTO_INCREMENT,
-    est_admin BOOLEAN,
-    est_adherent BOOLEAN,
-    est_superadmin BOOLEAN,
-    id_adherent INT,
-    id_admin INT,
-    id_superadmin INT,
-    FOREIGN KEY (id_admin) REFERENCES ADMIN(id_admin),
-    FOREIGN KEY (id_adherent) REFERENCES ADHERENT(id_adherent),
-    FOREIGN KEY (id_superadmin) REFERENCES SUPERADMIN(id_superadmin)
-); 
+CREATE TABLE ADMIN (
+    id_admin INT PRIMARY KEY AUTO_INCREMENT,
+    prenom VARCHAR(50) NOT NULL,
+    nom VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    telephone VARCHAR(20) NOT NULL,
+    date_naissance DATE NOT NULL,
+    mot_de_passe VARCHAR(255) NOT NULL,
+    id_utilisateur INT,
+    id_club INT,
+    FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur)
+    FOREIGN KEY (id_club) REFERENCES CLUB(id_club)
+);
+
+CREATE TABLE SUPERADMIN (
+    id_superadmin INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    telephone VARCHAR(20) NOT NULL,
+    mot_de_passe VArCHAR(255) NOT NULL,
+    id_utilisateur INT,
+    FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur)
+);
 
 CREATE TABLE ADHERENT (
     id_adherent INT PRIMARY KEY AUTO_INCREMENT,
@@ -36,20 +46,24 @@ CREATE TABLE ADHERENT (
     FOREIGN KEY (id_club) REFERENCES CLUB(id_club)
 );
 
+CREATE TABLE UTILISATEUR (
+    id_utilisateur INT PRIMARY KEY AUTO_INCREMENT,
+    est_admin BOOLEAN,
+    est_adherent BOOLEAN,
+    est_superadmin BOOLEAN,
+    id_adherent INT,
+    id_admin INT,
+    id_superadmin INT,
+    FOREIGN KEY (id_admin) REFERENCES ADMIN(id_admin),
+    FOREIGN KEY (id_adherent) REFERENCES ADHERENT(id_adherent),
+    FOREIGN KEY (id_superadmin) REFERENCES SUPERADMIN(id_superadmin)
+); 
+
 CREATE TABLE TERRAIN (
     id_terrain INT PRIMARY KEY AUTO_INCREMENT,
     adresse VARCHAR(255) NOT NULL,
     id_club INT
     FOREIGN KEY (id_club) REFERENCES CLUB(id_club)
-);
-
--- Permet a ce que plusieurs adhérent puissent réserver une même réservation.
-CREATE TABLE ADHERENT_RESERVATION (
-  id_adherent INT,
-  id_reservation INT,
-  PRIMARY KEY (id_adherent, id_reservation),
-  FOREIGN KEY (id_adherent) REFERENCES ADHERENT(id_adherent),
-  FOREIGN KEY (id_reservation) REFERENCES RESERVATION(id_reservation)
 );
 
 CREATE TABLE RESERVATION (
@@ -75,27 +89,13 @@ CREATE TABLE MATCH (
     FOREIGN KEY (id_reservation) REFERENCES RESERVATION(id_reservation)
 );
 
-CREATE TABLE ADMIN (
-    id_admin INT PRIMARY KEY AUTO_INCREMENT,
-    prenom VARCHAR(50) NOT NULL,
-    nom VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    telephone VARCHAR(20) NOT NULL,
-    date_naissance DATE NOT NULL,
-    mot_de_passe VARCHAR(255) NOT NULL,
-    id_utilisateur INT,
-    id_club INT,
-    FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur)
-    FOREIGN KEY (id_club) REFERENCES CLUB(id_club)
-);
-
-CREATE TABLE SUPERADMIN (
-    id_superadmin INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    telephone VARCHAR(20) NOT NULL,
-    mot_de_passe VArCHAR(255) NOT NULL,
-    id_utilisateur INT,
-    FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR(id_utilisateur)
+-- Permet a ce que plusieurs adhérent puissent réserver une même réservation.
+CREATE TABLE ADHERENT_RESERVATION (
+  id_adherent INT,
+  id_reservation INT,
+  PRIMARY KEY (id_adherent, id_reservation),
+  FOREIGN KEY (id_adherent) REFERENCES ADHERENT(id_adherent),
+  FOREIGN KEY (id_reservation) REFERENCES RESERVATION(id_reservation)
 );
 
 CREATE TABLE CHASUBLE(
