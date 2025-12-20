a81f758ebbaefea4950007c757e4c86a461bc4a0
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2025-12-20 14:17:55 +0100

    ajout du middleware

--- routes/licence.js
+++ routes/licence.js
@@ -1,9 +1,10 @@
 import express from "express"
+import cookieJwt from "../middleware/jwt.js"
 import { identifiantLicence } from "../controllers/identifiantLicence.js"
 
 const router = express.Router()
 
 // Chemins des routes -> controllers
-router.get("/identifiantLicence", identifiantLicence)
+router.get("/identifiantLicence", cookieJwt, identifiantLicence)
 
 export default router
