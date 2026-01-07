9fd32417d6e6444a06de01c36fe8c8111d4d2627
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2026-01-07 17:32:03 +0100

    ajout de l'import de cookieParser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import dépendances
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser";
 
 // Import des Routes
 import authRoutes from "./routes/auth.js"
@@ -11,12 +12,14 @@ const app = express()
 // ----- CORS ----- //// 
 // Options des Control Origin Request Sharing ou CORS
 const corsOptions = {
-  origin: '*',
+  origin: 'http://localhost:5173',
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }
 app.use(cors(corsOptions));
 
+app.use(cookieParser());
 
 app.use(express.json())
 
