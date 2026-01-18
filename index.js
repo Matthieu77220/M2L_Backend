57cf863073e139fede660baa22a702ade6b8163c
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2026-01-18 15:05:53 +0100

    ajout commentaire utile ne pas le supprimer

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/licence", licenceRoutes)
 app.use("/api/statistiqueAdherent", statistiqueAdherentRoutes)
 app.use("/api/superadmin", superadminRoutes)
+// rajouter les autres routes
 
 
 
