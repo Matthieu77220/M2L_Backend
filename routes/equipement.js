ad8ab320f9574f353c94882f7aede39d2a7a221e
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2026-01-20 14:13:29 +0100

    ajout des routes chasuble / ballon / crampon

--- routes/equipement.js
+++ routes/equipement.js
@@ -1,11 +1,13 @@
 import express from "express"
 import isAdmin from "../middleware/jwt.js"
-import { visualitionStock } from "../controllers/equipement.js"
+import { stockChasuble, stockCrampon, stockBallon } from "../controllers/equipement.js"
 
 const router = express.Router()
 
 // Chemins des routes -> controllers
-router.get("/visualitionStock", isAdmin, visualitionStock)
+router.get("/stockChasuble", isAdmin, stockChasuble)
+router.get("/stockCrampon", isAdmin, stockCrampon)
+router.get("/stockBallon", isAdmin, stockBallon)
 
 
 export default router
