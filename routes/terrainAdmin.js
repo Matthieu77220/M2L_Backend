58693da285cde2d6738461ab3ef819f666bd1e91
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2026-01-21 09:42:15 +0100

    ajout de la route SupprimerTerrain

--- routes/terrainAdmin.js
+++ routes/terrainAdmin.js
@@ -1,12 +1,13 @@
 import express from "express"
 import isAdmin from "../middleware/isAdmin.js"
-import { voirTerrain, ajouterTerrain } from "../controllers/terrain.js"
+import { voirTerrain, ajouterTerrain, supprimerTerrain } from "../controllers/terrain.js"
 
 const router = express.Router()
 
 // Chemins des routes -> controllers
 router.get("/voirTerrain", isAdmin , voirTerrain)
 router.post("/ajouterTerrain", isAdmin, ajouterTerrain)
+router.delete("/supprimerTerrain", isAdmin, supprimerTerrain)
 
 
 export default router
