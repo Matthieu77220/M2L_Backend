89a3e2277492f821c9589c789436b133be82ce18
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2026-01-23 18:20:12 +0100

    ajout de la fonction modifierTerrain

--- routes/terrainAdmin.js
+++ routes/terrainAdmin.js
@@ -1,6 +1,6 @@
 import express from "express"
 import isAdmin from "../middleware/isAdmin.js"
-import { voirTerrain, ajouterTerrain, supprimerTerrain } from "../controllers/terrain.js"
+import { voirTerrain, ajouterTerrain, supprimerTerrain, modifierTerrain } from "../controllers/terrain.js"
 
 const router = express.Router()
 
@@ -8,6 +8,7 @@ const router = express.Router()
 router.get("/voirTerrain", isAdmin , voirTerrain)
 router.post("/ajouterTerrain", isAdmin, ajouterTerrain)
 router.delete("/supprimerTerrain", isAdmin, supprimerTerrain)
+router.put("/modifierTerrain", isAdmin, modifierTerrain)
 
 
 export default router
