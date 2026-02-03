30f3af7c5f75b4b997ff1be3f62e28f3e7a16b4a
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2026-02-03 17:21:48 +0100

    ajout du middleware dans la route

--- routes/modifProfil.js
+++ routes/modifProfil.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { modificationProfile } from "../controllers/modifProfil.js"
+import cookieJwt from "../middleware/jwt.js"
 
 const router = express.Router()
 
-router.put("/modificationProfile", modificationProfile)
+router.put("/modificationProfile", cookieJwt, modificationProfile)
 
 export default router
