637186ef4389d36bf7173aa51fcb467c73ee3c4d
Nguyen Leclerc Eric <eric.nguyenleclerc@gmail.com>
2026-02-03 16:52:21 +0100

    ne touche plus a mon code Matthieu - si ca marche faut pas toucher

--- controllers/identifiantLicence.js
+++ controllers/identifiantLicence.js
@@ -2,26 +2,25 @@ import db from "../config/db.js"
 import 'dotenv/config'
 
 // ----- LICENCE ----- //
-export const identifiantLicence = async (req, res) => {
-    try {
-        // --- Récupération de l'id de l'adherent depuis le middleware ---
-        const id = req.user.id
+export const identifiantLicence = (req, res) => {
 
-        // --- Préparation de la requete préparée pour Afficher les informations de la licence ---
-        const sql = `SELECT a.prenom, a.nom, a.date_naissance, a.type_abonnement, l.numero_adherent, l.debut_licence, l.fin_licence
-                     FROM adherent as a
-                     JOIN licence as l on l.id_adherent = a.id_adherent
-                     WHERE l.id_adherent = ? `
+    // --- Récupération de l'id de l'adherent depuis le middleware ---
+    const id = req.user.id
 
-        const [result] = await db.query(sql, [id])
+    // --- Préparation de la requete préparée pour Afficher les informations de la licence ---
+    const sql = `SELECT a.prenom, a.nom, a.date_naissance, a.type_abonnement, l.numero_adherent, l.debut_licence, l.fin_licence
+                 FROM adherent as a
+                 JOIN licence as l on l.id_adherent = a.id_adherent
+                 WHERE l.id_adherent = ? `
 
-        if (result.length === 0) {
-            return res.status(404).send("Aucune licence trouvée pour cet adhérent.")
+    db.query(sql, id, (err, result) => {
+        if (err) {
+            return res.status(500).send("Erreur lors de l'éxecution de la requêtes SQl.")
         }
 
-        return res.json([result[0]])
-    } catch (err) {
-        console.error("Erreur lors de la récupération de la licence:", err)
-        return res.status(500).send("Erreur lors de l'exécution de la requête SQL.")
-    }
+        if (result) {
+            return res.send([result[0]])
+        }
+    })
+
 }
--- controllers/statistiqueAdherent.js
+++ controllers/statistiqueAdherent.js
@@ -2,45 +2,48 @@ import db from "../config/db.js"
 import 'dotenv/config'
 
 // ----- Statistique ----- //
-export const statistique = async (req, res) => {
-    try {
-        // --- Récupération de l'id de l'adherent depuis le middleware ---
-        const id = req.user.id
+export const statistique = (req, res) => {
 
-        const sql = `SELECT COUNT(m.id_match) AS nombreMatch , SUM(m.nb_victoires) AS victoire , SUM(m.nb_defaites) AS defaite , SUM(m.nb_egalites) AS egalite
-                     FROM matchs as m
-                     JOIN reservation r on r.id_reservation = m.id_reservation
-                     JOIN adherent_reservation ad on ad.id_reservation =  r.id_reservation 
-                     where ad.id_adherent = ? `
-        
-        // --- Requete préparée ---
-        const [results] = await db.query(sql, [id])
+    // --- Récupération de l'id de l'adherent depuis le middleware ---
+    const id = req.user.id
 
-        return res.json([results[0]])
-    } catch (err) {
-        console.error("Erreur lors de la récupération des statistiques:", err)
-        return res.status(500).send("Erreur lors de l'exécution de la requete SQL.")
-    }
+    const sql =  `SELECT COUNT(m.id_match) AS nombreMatch , SUM(m.nb_victoires) AS victoire , SUM(m.nb_defaites) AS defaite , SUM(m.nb_egalites) AS egalite
+                  FROM matchs as m
+                  JOIN reservation r on r.id_reservation = m.id_reservation
+                  JOIN adherent_reservation ad on ad.id_reservation =  r.id_reservation 
+                  where ad.id_adherent = ? `
+    
+    // --- Requete préparée ---
+    db.query(sql, id, (err, results) => {
+
+        if (err) {
+            return res.status(500).send("Erreur lors de l'exécution de la requete SQL.")
+        }else {
+            return res.json([results[0]])
+        }
+    })
+
+
 }
 
 // ----- Visualisation Match ----- //
-export const visualisationMatch = async (req, res) => {
-    try {
-        // --- Récupération de l'id de l'adherent depuis le middleware ---
-        const id = req.user.id
+export const visualisationMatch = (req, res) => {
 
-        // A FAIRE !!!!  apres les AS rennomer selon le map dans le front
-        const sql = `SELECT r.date_reservation, m.score, m.status
-                     FROM reservation r
-                     JOIN matchs m on m.id_reservation = r.id_reservation
-                     JOIN adherent_reservation ad on r.id_reservation = ad.id_reservation
-                     WHERE ad.id_adherent = ? `
+    // --- Récupération de l'id de l'adherent depuis le middleware ---
+    const id = req.user.id
 
-        const [results] = await db.query(sql, [id])
-        
-        return res.json(results)
-    } catch (err) {
-        console.error("Erreur lors de la récupération des matchs:", err)
-        return res.status(500).send("Erreur lors de l'exécution de la requete SQL.")
-    }
+    // A FAIRE !!!!  apres les AS rennomer selon le map dans le front
+    const sql = `SELECT r.date_reservation, m.score, m.status
+                 FROM reservation r
+                 JOIN matchs m on m.id_reservation = r.id_reservation
+                 JOIN adherent_reservation ad on r.id_reservation = ad.id_reservation
+                 WHERE ad.id_adherent = ? `
+
+    db.query(sql, id, (err, results) => {
+        if (err) {
+            return res.status(500).send("Erreur lors de l'exécution de la requete SQL.")
+        }else {     
+            return res.json(results)
+        }
+    })
 }
